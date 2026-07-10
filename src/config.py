7a60892b49971293ffc7bd877a614e4aaee21857
Felipe Quérette <felipe.querette@gmail.com>
2026-07-10 01:15:14 -0300

    Extract selic and usd from Bacen API

--- src/config.py
+++ src/config.py
@@ -7,3 +7,7 @@ from pathlib import Path
 ROOT_DIR = Path(__file__).resolve().parent.parent
 
 RAW_FILE = ROOT_DIR / "data" / "raw" / "leiloes_tesouro_nacional.csv"
+
+START_DATE = "01/01/2015"
+
+END_DATE = "31/12/2023"
